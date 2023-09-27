502806fc9d53e487c15b4c19380b809e3f67f5b1
PieselKlif <pieselklif@gmail.com>
2023-09-27 19:56:38 +0200

    panel phone fix

--- src/js/main.js
+++ src/js/main.js
@@ -5,12 +5,14 @@ function toggleMenu() {
 	if (document.getElementById("left-panel").className == "left-panel close")
 	{
 		document.getElementById("left-panel").classList.remove('close');
+		document.getElementById("menu-button").classList.add('open');
 		if (!isPhone)
 		{
 			document.getElementById("content").classList.remove('full');
 		}
 	}else{
 		document.getElementById("left-panel").classList.add('close');
+		document.getElementById("menu-button").classList.remove('open');
 		if (!isPhone)
 		{
 			document.getElementById("content").classList.add('full');
@@ -18,16 +20,13 @@ function toggleMenu() {
 	}
 }
 
-// TODO naprawic lewy panel na telefonach
-
 if (screen.width <= 700)
 {
 	isPhone = true;
-
-	// document.getElementById("left-panel").style.display = "none";
 } else {
 	document.getElementById("content").classList.remove('full');
 	document.getElementById("left-panel").classList.remove('close');
+	document.getElementById("menu-button").classList.add('open');
 }
 
 document.getElementById("menu-button").addEventListener("click", toggleMenu);
