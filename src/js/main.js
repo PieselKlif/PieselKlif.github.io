cd5c4334bf0e0dec93fdce26cfb40631231364be
PieselKlif <pieselklif@gmail.com>
2023-09-27 17:58:07 +0200

    animacja panelu

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,21 @@
 var menuIsOpen = true;
 
 function toggleMenu() {
-	//  TODO zrobić jakąś animacje otwierania i zamykania. Może dawać left-panel display jako absolute, i tak nie będzie widać.
-
-	if (document.getElementById("left-panel").style.display == "none")
+	if (document.getElementById("left-panel").className == "left-panel close")
 	{
-		document.getElementById("left-panel").style.display = "block";
+		document.getElementById("left-panel").classList.remove('close');
+		document.getElementById("content").classList.remove('full');
 	}else{
-		document.getElementById("left-panel").style.display = "none";
+		document.getElementById("left-panel").classList.add('close');
+		document.getElementById("content").classList.add('full');
 	}
 }
 
-if (screen.width <= 700)
-{
-	document.getElementById("left-panel").style.display = "none";
-} else {
-	document.getElementById("left-panel").style.display = "block";
-}
+// if (screen.width <= 700)
+// {
+// 	document.getElementById("left-panel").style.display = "none";
+// } else {
+// 	document.getElementById("left-panel").style.display = "block";
+// }
 
 document.getElementById("menu-button").addEventListener("click", toggleMenu);
