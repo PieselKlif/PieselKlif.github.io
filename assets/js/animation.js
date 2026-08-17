aa63971d82ceb888952e6d5b461b0efe093ac779
PieselKlif <pieselklif@gmail.com>
2026-08-17 19:20:51 +0200

    add scroll animation

---
+++ assets/js/animation.js
@@ -0,0 +1,18 @@
+const observerOptions = {
+	root: null,
+	rootMargin: '0px 0px -15% 0px',
+	threshold: 0
+};
+
+const observer = new IntersectionObserver((entries) => {
+	entries.forEach(entry => {
+		if (entry.isIntersecting) {
+			entry.target.classList.add('is-visible');
+			observer.unobserve(entry.target);
+		}
+	});
+}, observerOptions);
+
+document.querySelectorAll('.scroll-reveal').forEach(e => {
+	observer.observe(e);
+});
